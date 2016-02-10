74873cad2faec74a2caa23d0b7e21c2cc565fc91
David Taylor <David.Taylor@wavetronix.com>
2016-02-10 12:47:36 -0700

    initial commit

---
+++ Assets/MonsterData.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class MonsterLevel
+{
+    public GameObject bullet;
+    public float fireRate;
+    public int cost;
+    public GameObject visualization;
+}
+public class MonsterData : MonoBehaviour {
+    private MonsterLevel currentLevel;
+    public List<MonsterLevel> levels;
+    public MonsterLevel CurrentLevel
+    {
+        //2
+        get
+        {
+            return currentLevel;
+        }
+        //3
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+    public MonsterLevel getNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else {
+            return null;
+        }
+    }
+    public void increaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
